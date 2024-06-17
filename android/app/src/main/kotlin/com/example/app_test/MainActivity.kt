6793c35cab0e01e054b85da2d5eacebe3e1191e6
CleversonAbreu <crehbatera@gmail.com>
2024-06-17 10:46:19 -0300

    splash android

--- android/app/src/main/kotlin/com/example/app_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/app_test/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.example.app_test
 
+
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.NormalTheme) // Define o tema normal após o Flutter iniciar
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:app_test/modules/app_module.dart';
 import 'package:app_test/modules/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(ModularApp(
     module: AppModule(),
     child: const AppWidget(),
