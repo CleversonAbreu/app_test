c1c314eccb6e8bd3910a2a5a21165471c64befaf
CleversonAbreu <crehbatera@gmail.com>
2025-01-14 19:53:45 -0300

    feat(auth): auth

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -6,10 +6,32 @@ class DioClient {
   final Dio dio;
 
   DioClient(FlutterSecureStorage secureStorage) : dio = Dio() {
-    dio.options.baseUrl = 'https://apptest.free.beeceptor.com/';
+    // Dio standard configurations
+    dio.options.baseUrl = 'http://192.168.1.5:8080/api/v1/';
     dio.options.connectTimeout = const Duration(seconds: 10);
     dio.options.receiveTimeout = const Duration(seconds: 10);
+    dio.options.followRedirects = true;
+    dio.options.validateStatus = (status) {
+      // Allows redirections and avoids failures in 3xx codes
+      return status != null && status < 500;
+    };
 
+    // Default header for requests
+    dio.options.headers = {
+      'Content-Type': 'application/json',
+      "Accept": "application/json",
+    };
+
+    // Add authentication interceptor
     dio.interceptors.add(AuthInterceptor(secureStorage));
+
+    // Add a log interceptor for debugging
+    dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestHeader: true,
+      responseHeader: true,
+      responseBody: true,
+      error: true,
+    ));
   }
 }
--- lib/modules/auth/authentication/data/datasources/auth_datasource_impl.dart
+++ lib/modules/auth/authentication/data/datasources/auth_datasource_impl.dart
@@ -18,7 +18,7 @@ class AuthDataSourceImpl implements AuthDatasource {
     try {
       final response = await dioClient.dio.post(
         '/login',
-        data: authModel,
+        data: authModel.toJson(),
       );
       if (response.statusCode == 200) {
         return ResultAuthModel.fromMap(response.data);
@@ -36,5 +36,5 @@ class AuthDataSourceImpl implements AuthDatasource {
     } catch (e) {
       throw UnknownException('An unexpected error occurred: $e');
     }
-  }
+  }s
 }
